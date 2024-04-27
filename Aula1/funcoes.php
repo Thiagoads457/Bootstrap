f019688d0efc71c84cbc4cb03e6c23d8f7949df2
Thiagoads457 <thiagoads457@gmail.com>
2024-04-27 11:03:03 -0300

    1103

---
+++ Aula1/funcoes.php
@@ -0,0 +1,22 @@
+<?php
+    function exibirData(){
+        echo "Hoje é dia ".date("d");
+    } 
+
+    function somar($n1, $n2){
+        return $n1 + $n2;
+    
+   }
+
+   function subtrair($n1, $n2){
+    return $n1 - $n2;
+   }
+
+   function multiplicar($n1, $n2){
+    return $n1 * $n2;
+   }
+
+   function dividir($n1, $n2){
+    return $n1 / $n2;
+   }
+?>
